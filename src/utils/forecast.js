7cd06e17fe91418deeb3567ef31bd8e8d0fff701
Anussara Promadee <sara@Anussaras-MacBook-Air.local>
2022-01-17 15:43:53 +0000

    Add local time to Weather page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,9 +3,8 @@ const request = require('request')
 
 const forecast =(latitude,longtitude,callback)=>{
     const url = 'http://api.weatherstack.com/current?access_key=d752dde4fc101d46c7ae5b756eb1a682&query='+ latitude +','+longtitude  //'50.7183,-1.8833'
-   //const url = 'http://api.weatherstack.com/current?access_key=d752dde4fc101d46c7ae5b756eb1a682&query='+ location //'50.7183,-1.8833'
-    
-request({url:url, json:true},(error,{body})=>{ //this was responce but change to {body} shorter object 
+   
+    request({url:url, json:true},(error,{body})=>{ //this was responce but change to {body} shorter object 
     if(error){
         callback('Unable to connect to the weather service!',undefined)
 
@@ -16,7 +15,7 @@ request({url:url, json:true},(error,{body})=>{ //this was responce but change to
         callback(undefined,
             
             'Today weather: ' + body.current.weather_descriptions[0] + '. It is currently ' 
-            + body.current.temperature + ' degree.' + ' But it feels like ' + body.current.feelslike + ' degree out there',
+            + body.current.temperature + ' degree.' + ' But it feels like ' + body.current.feelslike + ' degree out there ' + 'Current local time: ' +body.location.localtime,
             
             
         )
