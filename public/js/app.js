a446e9033ddfbfd7ff2145fa1bf940a555d398b3
Sara <88390308+AnussaraP@users.noreply.github.com>
2022-03-04 11:21:19 +0000

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-// console.log("Client sidee JavaScript is loaded!");
-
-//Fetch API is not part of JavaScript but it is a brower based API,
-//its something we can use in all modern browers but not accessable in node js
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
 
 const weatherForm = document.querySelector('form')  //this is target  by element but it will match the first one that show 
 const search = document.querySelector('input')   //first input or form  but if want to target b y class can so as ('.className=')
@@ -44,25 +33,5 @@ fetch("/weather?address="+ location )  //whateve value store in location above
       })
   })
 
-    
-//     fetch("http://localhost:3000/weather?address="+ location )  //whateve value store in location above
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Request Error");
-//     }
-//     response
-//       .json()
-//       .then((data) => { 
-//         // if (data.error) {
-//         //   throw new Error(data.error);
-//         // }
-//         console.log(data.forecast);
-//         console.log(data.address);
-//       })
-//   })
-// .catch((err) => {
-//     console.log({ err })
-//   })
-    
-// })
+   
 
