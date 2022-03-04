0a7484ed2055bd2f6033470f435861241a3c81aa
Sara <88390308+AnussaraP@users.noreply.github.com>
2022-03-04 11:22:48 +0000

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,8 +4,6 @@ const request = require('request')
 const geocode = (address,callback)=>{
 const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiYXByb21kZWUiLCJhIjoiY2t5MGw4Z3loMDJxaDJ1cG4zc3N0YmZ0NCJ9.ew8wjwqgXvNzbnIUBUsfxQ&limit=1'
 
-    //const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiYXByb21kZWUiLCJhIjoiY2t5MGw4Z3loMDJxaDJ1cG4zc3N0YmZ0NCJ9.ew8wjwqgXvNzbnIUBUsfxQ&limit=1' //encodeURIComponent() => this is function, allow special charactors 
-      
 request({ url, json:true }, (error,{body})=>{       //it was response but change to {body}, make it shorter 
     if(error){
         callback('Unable to connect to location services', undefined)   //error = undefined will show because thr is no error, if there is error will shoe the text 
@@ -24,20 +22,7 @@ request({ url, json:true }, (error,{body})=>{       //it was response but change
 
 })
 }
-// geocode('Bournemouth',(error,data)=> {    //address=> 'New York', callback = (error,data)
-//     console.log('Error',error)
-//     console.log('Data',data)
 
-// })  //this is callback function after 'UK' (( )=>{ })
-
-
-
-
-
-
-
-
-//&limit=1
 
 module.exports= geocode
 
